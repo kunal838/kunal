ee3791284dfcb229427f7a386f0e5fdab3ec8cbd
kunal patel <pkunal257@gmail.com>
2021-03-29 17:43:16 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import { Box ,Text} from "@chakra-ui/react"
+import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,38 +12,46 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <Text
+  bgGradient="linear(to-l,#ff00cc,#333399)"
+  bgClip="text"
+  fontSize="6xl"
+  fontWeight="extrabold"
+>
+ Welcome Enter
+ with a happy  <Image src={'/2.svg'} height={50} width={100}/>Heart
+</Text> 
         </h1>
+        
+        <Image src={'/1.svg'} height={650} width={500}/>
+        
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
+            <h3>Home Automation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
+          <a href="https://twitter-clone-3c380.web.app/" className={styles.card}>
+            <h3>Twitter frontend &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://clone-7bb36.web.app/"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
+            <h3>Youtube Frontend &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/kunal838/"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Github &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
@@ -51,14 +60,15 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <Text
+  bgGradient="linear(to-l, #7928CA,#FF0080)"
+  bgClip="text"
+  fontSize="1xl"
+  fontWeight="extrabold"
+>
+ Open Source --> Kunal patel
+</Text>
+
       </footer>
     </div>
   )
